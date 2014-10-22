906197755feec73c1fb76b35015e2d233fd6785b
k.alnajar <k.alnajar@opower.com>
2014-10-22 16:28:15 -0400

    updating my work

---
+++ Game_of_Life/game_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require 'require'
+
+describe Game do
+  context Board do
+    it 'should give me the current board configuration' do
+      b = Board.new(5)
+      expect(b.grid.size).to eq 5
+    end
+
+  context Cell do
+    it 'should give me the state of a cell' do
+      c = Cell.new(Cell.ALIVE)
+      expect(c.current_state).to be_truthy
+    end
+  end
+
+    it 'should give me the state of a cells neighbors'
+  end
+end
